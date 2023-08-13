73f2e4d31eb05abf64dad63b3a08bd947ffdef3f
hehenischal <nischallc56@gmail.com>
2023-08-13 19:25:41 +0545

    all but images

--- home/models.py
+++ home/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
+    
     image = models.ImageField(upload_to='images/')
-
-
-    def __str__(self):
-        return self.image.name
+    uploaded_at = models.DateTimeField(auto_now_add=True, blank= True, null=True)
--- home/views.py
+++ home/views.py
@@ -1,39 +1,48 @@
 import requests
 from django.shortcuts import render
 from api_keys import API_KEY
+from .forms import ImageForm
 
 def index(request):
     if request.method == 'POST':
-        image = request.FILES.get('image')
-
-        if image:
-            url = 'https://api.ocr.space/parse/image'
-            payload = {
+        
+            image = request.FILES.get('image')
+            url = "https://api.ocr.space/parse/image"
+            payload = {}
+            files = [
+                ('', (image.name, image.file, image.content_type))
+            ]
+            headers = {
                 'apikey': API_KEY,
             }
+            response = requests.post(url, headers=headers, data=payload, files=files)
             
-            files = {
-                'file': (image.name, image, image.content_type),
-            }
-
-            response = requests.post(url, data=payload, files=files)
             data = response.json()
             print(data)
-
-            if 'ParsedResults' in data and len(data['ParsedResults']) > 0:
-                extracted_text = data['ParsedResults'][0]['ParsedText']
-                context = {
-                    'extracted_text': extracted_text,
-                    'image': image,
-                }
-                return render(request, 'index.html', context)
+            
+            parsed_results = data.get('ParsedResults', [])
+            if parsed_results:
+                extracted_text = parsed_results[0].get('ParsedText', 'No text found!')
             else:
-                error_message = "Failed to extract text. Please check the uploaded image."
-                context = {
-                    'error_message': error_message,
-                    'image': image,
-                }
-                return render(request, 'index.html', context)
+                extracted_text = 'No text found!'
+            form = ImageForm(request.POST, request.FILES)
+            if form.is_valid():
+                image = form.cleaned_data['image']
+                print("form is valid & image size is" + 
+                      str(image.size) + "it's name is " + 
+                      image.name + "and it's content type is " + 
+                      image.content_type + "it's location is " + 
+                      str(image.file))
+                form.save()
+            
+            context = {
+                'extracted_text': extracted_text,
+                'form' : form,
+            }
+            return render(request, 'index.html', context)
 
-    return render(request, 'index.html')
-
+    form = ImageForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'index.html', context)
