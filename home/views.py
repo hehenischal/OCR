0f9576da9818c78a88f951b342c37f8368127aea
hehenischal <nischallc56@gmail.com>
2023-08-14 11:35:31 +0545

    idk

--- home/views.py
+++ home/views.py
@@ -23,8 +23,31 @@ def index(request):
             parsed_results = data.get('ParsedResults', [])
             if parsed_results:
                 extracted_text = parsed_results[0].get('ParsedText', 'No text found!')
+                text_overlay = parsed_results[0].get('TextOverlay', {})
+                lines = text_overlay.get('Lines', [])
+            
+                if lines:
+                    # Calculate the bounding box of the detected text
+                    x_coords = [line['Words'][0]['Left'] for line in lines]
+                    y_coords = [line['Words'][0]['Top'] for line in lines]
+                    min_x = min(x_coords)
+                    max_x = max(x_coords)
+                    min_y = min(y_coords)
+                    max_y = max(y_coords)
+                
+                    # Calculate overlay position and size based on bounding box
+                    overlay_left = min_x
+                    overlay_top = min_y
+                    overlay_width = max_x - min_x
+                    overlay_height = max_y - min_y
+                
+                    context['overlay_left'] = overlay_left
+                    context['overlay_top'] = overlay_top
+                    context['overlay_width'] = overlay_width
+                    context['overlay_height'] = overlay_height
             else:
                 extracted_text = 'No text found!'
+            
             form = ImageForm(request.POST, request.FILES)
             if form.is_valid():
                 image = form.cleaned_data['image']
