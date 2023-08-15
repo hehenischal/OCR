60e3685ffcb52c5f20f3ebf867a7124e2096b63f
hehenischal <nischallc56@gmail.com>
2023-08-15 07:33:37 +0545

    added my domain to allowed hosts

--- OCR/settings.py
+++ OCR/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', ]
+ALLOWED_HOSTS = ['.vercel.app','nischallamichhane.com.np' ]
 
 
 # Application definition
