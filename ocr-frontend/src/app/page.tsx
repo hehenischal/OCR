c9022c8422d660467665c0049006a885e39a3742
saagarbhujel <saagarbhujel22@gmail.com>
2023-08-16 22:07:14 +0545

    home item imported

--- ocr-frontend/src/app/page.tsx
+++ ocr-frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
+import HomeItem from '@/components/homeItem'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
-      
+      <HomeItem/>
     </main>
   )
 }
