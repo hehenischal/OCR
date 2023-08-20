20ee73cccd079b0e7b0a2d79390f94f500505a38
saagarbhujel <saagarbhujel22@gmail.com>
2023-08-20 10:10:55 +0545

    contact component done

--- ocr-frontend/src/app/contact/page.tsx
+++ ocr-frontend/src/app/contact/page.tsx
@@ -2,8 +2,56 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
+    <section className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded shadow-md w-96">
+        <h1 className="text-2xl font-semibold mb-4">Contact Us</h1>
+        <form 
+          action="https://formsubmit.co/23e2a2832856c4f4bc41ec9c61bb1c58"
+          method="POST"
+        >
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-gray-600">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full border rounded px-3 py-2 focus:outline-none focus:border-blue-400"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full border rounded px-3 py-2 focus:outline-none focus:border-blue-400"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="message" className="block text-gray-600">
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              rows="4"
+              className="w-full border rounded px-3 py-2 focus:outline-none focus:border-blue-400 resize-none"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact
